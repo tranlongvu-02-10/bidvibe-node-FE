5f2156f965d1ca345e670404ae5224f61c2b8789
tranlongvu-02-10 <tranlongvu02102004@gmail.com>
2026-03-28 22:19:01 +0700

    chore: init vite react ts project with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        purple: {
+          50:  '#EEEDFE',
+          100: '#CECBF6',
+          200: '#AFA9EC',
+          400: '#7F77DD',
+          600: '#534AB7',
+          800: '#3C3489',
+          900: '#26215C',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
